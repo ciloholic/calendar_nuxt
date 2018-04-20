f21b7d7a77937d37f012dba17fecc8dbc79198e9
ciloholic <ciloholic0119@gmail.com>
2018-04-21 08:02:38 +0900

    ヘッダーを追加

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,11 @@
 <template>
-  <el-header height="100vh">Header</el-header>
+  <el-header height="100vh">
+    <ul>
+      <li>
+        <nuxt-link to="/"><img src="/calendar.png">稼働入力</nuxt-link>
+      </li>
+    </ul>
+  </el-header>
 </template>
 
 <style lang="scss" scoped>
@@ -7,5 +13,29 @@
   background: #dd8a61;
   grid-area: header;
   padding: 0;
+
+  a {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #332a25;
+    width: 80px;
+    height: 80px;
+    text-decoration: none;
+    font-size: 11px;
+    letter-spacing: 2px;
+
+    img {
+      display: block;
+      margin-bottom: 10px;
+      width: 30%;
+    }
+  }
+
+  a.active,
+  a:hover {
+    background: rgba(0, 0, 0, 0.3);
+  }
 }
 </style>
